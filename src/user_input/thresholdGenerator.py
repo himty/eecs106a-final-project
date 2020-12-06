bcaea19c9b1b766185048f6c379b302f8063cf1b
Jessie Mindel <jessie.mindel@gmail.com>
2020-12-06 14:12:00 -0800

    Add ability to read multiple files in a folder to threshold generator

--- src/user_input/thresholdGenerator.py
+++ src/user_input/thresholdGenerator.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import sys
+import os
 
 def generateThresholds(bgrImg, threshold=0, verbose=False):
 	hsvImg = cv2.cvtColor(bgrImg, cv2.COLOR_BGR2HSV)
@@ -18,15 +19,61 @@ def generateThresholds(bgrImg, threshold=0, verbose=False):
 	if verbose: print(f"Min: ({hMin}, {sMin}, {vMin})\nMax: ({hMax}, {sMax}, {vMax})")
 	return ((hMin, sMin, vMin), (hMax, sMax, vMax))
 
+def generateThresholdsMany(bgrImgs, threshold=0, verbose=False):
+	if not bgrImgs or len(bgrImgs) == 0:
+		return None, None
+
+	hMin = np.inf
+	hMax = -1
+	sMin = np.inf
+	sMax = -1
+	vMin = np.inf
+	vMax = -1
+
+	for bgrImg in bgrImgs:
+		if verbose: print("Running on a new image...")
+		hsvMin, hsvMax = generateThresholds(bgrImg, threshold, verbose=verbose)
+
+		if hsvMin[0] < hMin:
+			hMin = hsvMin[0]
+		if hsvMin[1] < sMin:
+			sMin = hsvMin[1]
+		if hsvMin[2] < vMin:
+			vMin = hsvMin[2]
+
+		if hsvMax[0] > hMax:
+			hMax = hsvMax[0]
+		if hsvMax[1] > sMax:
+			sMax = hsvMax[1]
+		if hsvMax[2] > vMax:
+			vMax = hsvMax[2]
+
+	if verbose: print(f"\nMin: ({hMin}, {sMin}, {vMin})\nMax: ({hMax}, {sMax}, {vMax})")
+	return ((hMin, sMin, vMin), (hMax, sMax, vMax))
+
 if __name__ == '__main__':
 	if len(sys.argv) == 0:
 		print('Usage: thresholdGenerator.py <filename> <threshold>\nInput an image file, all of whose pixels are black, except for the pixels whose colors you\'d like to threshold.\nThreshold is the minimum value required for the item to be considered to be not a black pixel. 0 by default.')
 		sys.exit(1)
 
-	img = cv2.imread(sys.argv[1])
 	threshold = 0
-	if sys.argv[2]:
+	if len(sys.argv) >= 3 and sys.argv[2]:
 		threshold = int(sys.argv[2])
 
-	print(f"Finding color thresholding bounds with minimum value {threshold} for masked file \"{sys.argv[1]}\"...")
-	generateThresholds(img, threshold=threshold, verbose=True)
+	filename = sys.argv[1]
+	if os.path.isfile(filename):
+		img = cv2.imread(filename)
+		print(f"Finding color thresholding bounds with minimum value {threshold} for masked file \"{filename}\"...")
+		generateThresholds(img, threshold=threshold, verbose=True)
+	elif os.path.isdir(filename):
+		filenames = [os.path.join(filename, f) for f in os.listdir(filename) if os.path.isfile(os.path.join(filename, f)) and f[0] != '.']
+		imgs = []
+		for f in filenames:
+			imgs.append(cv2.imread(f))
+		print(f"Finding color thresholding bounds with minimum value {threshold} for the following masked files:")
+		for f in filenames:
+			print(f)
+		print()
+		generateThresholdsMany(imgs, threshold=threshold, verbose=True)
+	else:
+		print(f"Failed to threshold. The inputted path is neither a file nor a directory.")
