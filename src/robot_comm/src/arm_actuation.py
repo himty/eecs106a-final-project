1feb8d66028a83be90b241991cbd7b999590dd4a
Jon Como <jon@underground.net>
2020-12-08 22:54:22 -0500

    arm actuation working sorta

--- src/robot_comm/src/arm_actuation.py
+++ src/robot_comm/src/arm_actuation.py
@@ -5,17 +5,22 @@ ROS network for path planning and output
 """
 
 import rospy
-from path_planning.moveit_planner.kinematics_calculator_moveit import KinematicsCalculator
+import sys
+from sensor_msgs.msg import JointState
+sys.path.insert(0,'/home/jon/ros_workspaces/eecs106a-final-project/src/path_planning/moveit_planner')
+
+print(sys.path)
+
+from robot_comm_msg.msg import AngleArr
+from kinematics_calculator_moveit import KinematicsCalculator
 
 #import user_input.vision as cv
 
 import numpy as np
 
-from robot_comm_msg.msg import AngleArr
-
 joint_states = [0, 0, 0]
 
-target_coords_ef = [0, 0, 0]
+target_coords_ef = np.array([0, 0, 0, 1])
 
 def updateJoints(data):
     joint_states = data.position
@@ -28,10 +33,10 @@ def cmd_angle():
 
     #callibration sequence launched from CV node
 
-    rospy.Subscriber("joint_states", String, updateJoints)
+    rospy.Subscriber("joint_states", JointState, updateJoints)
 
-    #placeholder topic 'spheres', update later
-    rospy.Subscriber("spheres", String, updateSpheres)
+    # placeholder topic 'spheres', update later
+    # rospy.Subscriber("spheres", String, updateSpheres)
 
     pub = rospy.Publisher('cmd_angle', AngleArr, queue_size=10)
     
@@ -50,8 +55,10 @@ def cmd_angle():
             #convert back to spatial g*coordinates
             target_coords_spatial = np.matmul(g, target_coords_ef)
 
-            angles = arm.inverse_kinematics(target_coords_spatial) 
-            
+            angles = arm.inverse_kinematics(target_coords_spatial[:3]) 
+            print(angles)
+
+            angles = angles.astype(np.int16).tolist()
             print(angles)
 
             pub_string = AngleArr(angles, rospy.get_time())
@@ -65,8 +72,8 @@ def cmd_angle():
 if __name__ == '__main__':
 
     rospy.init_node('cmd_angle', anonymous=True)
-    rospy.init_node('joint_listener', anonymous=True)
-    rospy.init_node('cv_listener', anonymous=True)
+    #rospy.init_node('joint_listener', anonymous=True)
+    #rospy.init_node('cv_listener', anonymous=True)
 
     try:
         cmd_angle()
