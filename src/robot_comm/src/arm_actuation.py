7b27f412ad093680ca654c62a60b7888d3ea88d5
Jenny Wang <jenny.wang244@gmail.com>
2020-12-08 21:03:51 -0700

    pub joint angles

--- src/robot_comm/src/arm_actuation.py
+++ src/robot_comm/src/arm_actuation.py
@@ -6,13 +6,17 @@ ROS network for path planning and output
 
 import rospy
 import sys
+from std_msgs.msg import Header
 from sensor_msgs.msg import JointState
 sys.path.insert(0,'/home/jon/ros_workspaces/eecs106a-final-project/src/path_planning/moveit_planner')
+sys.path.insert(0,'/home/jon/ros_workspaces/eecs106a-final-project/src')
 
 print(sys.path)
 
 from robot_comm_msg.msg import AngleArr
 from kinematics_calculator_moveit import KinematicsCalculator
+
+from path_planning.moveit_planner.next_pt_planner import NextPointPlanner
 
 #import user_input.vision as cv
 
@@ -20,21 +24,31 @@ import numpy as np
 
 joint_states = [0, 0, 0]
 
-target_coords_ef = np.array([0, 0, 0, 1])
+curr_sphere_pos = np.array([0, 0, 0, 1])
 
 def updateJoints(data):
     joint_states = data.position
 
 def updateSpheres(data):
     #update with correct message type
-    target_coords_ef = data.position
+    curr_sphere_pos = data.position
+
+def publish_joint_angles(pub, joint_angles):
+    joint_state = JointState()
+    joint_state.header = Header()
+    joint_state.header.stamp = rospy.Time.now()
+    joint_state.name = ['base_rotate', 'joint1', 'joint2']
+    joint_state.position = joint_angles
+    pub.publish(joint_state)
 
 def cmd_angle():
 
     #callibration sequence launched from CV node
 
     rospy.Subscriber("joint_states", JointState, updateJoints)
+    joint_pub = rospy.Publisher("joint_states", JointState, queue_size=10)
 
+    publish_joint_angles(joint_pub, [0, 0, 0])
     # placeholder topic 'spheres', update later
     # rospy.Subscriber("spheres", String, updateSpheres)
 
@@ -44,6 +58,8 @@ def cmd_angle():
 
     arm = KinematicsCalculator('arm')
 
+    next_pt_planner = NextPointPlanner(max_dist_per_timestep=2)
+
     while not rospy.is_shutdown():
 
         raw_input('Press enter to actuate arm:')
@@ -52,10 +68,16 @@ def cmd_angle():
 
             g = arm.forward_kinematics(joint_states)
 
-            #convert back to spatial g*coordinates
-            target_coords_spatial = np.matmul(g, target_coords_ef)
+            arm_pos = g[:3, 3]
 
-            angles = arm.inverse_kinematics(target_coords_spatial[:3]) 
+            #convert back to spatial g*coordinates
+            sphere_coords_spatial = np.matmul(g, curr_sphere_pos)[:3]
+
+            target_coords_spatial = next_pt_planner.get_near_point(arm_pos, sphere_coords_spatial):
+
+            angles = arm.inverse_kinematics(target_coords_spatial) 
+
+            publish_joint_angles(joint_pub, angles)
             print(angles)
 
             angles = angles.astype(np.int16).tolist()
