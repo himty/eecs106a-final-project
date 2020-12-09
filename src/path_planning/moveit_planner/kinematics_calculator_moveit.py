c00a8cf744828a6aa9da5c0d87f3b73aa2f9cfc2
Jenny Wang <jenny.wang244@gmail.com>
2020-12-08 20:23:19 -0700

    change ik fk to degrees

--- src/path_planning/moveit_planner/kinematics_calculator_moveit.py
+++ src/path_planning/moveit_planner/kinematics_calculator_moveit.py
@@ -44,15 +44,14 @@ class KinematicsCalculator():
     self.planner = PathPlanner(group_name)
     rospy.sleep(0.5)
 
-  def inverse_kinematics(self, pos, or_quaternion):
+  def inverse_kinematics(self, pos):
     """
     Calls the planner to get joint gives given a position.
     That unfortunately returns a trajectory of many points,
     so we take the last point
 
     pos- [x, y, z] in cm
-    or_quaternion- [or_x, or_y, or_z, or_w]. Target orientation in
-                  quaternions
+    or_euler- [roll, pitch, yaw]. Target orientation in euler angles in degrees
 
     Returns joint angles [theta1, theta2, theta3, theta4] in radians
     """
@@ -65,16 +64,13 @@ class KinematicsCalculator():
     goal.pose.position.y = pos[1]
     goal.pose.position.z = pos[2]
 
-    #Orientation as a quaternion
-    goal.pose.orientation.x = or_quaternion[0]
-    goal.pose.orientation.y = or_quaternion[0]
-    goal.pose.orientation.z = or_quaternion[0]
-    goal.pose.orientation.w = or_quaternion[0]
-
     plan = self.planner.plan_to_pose(goal, orien_const)
 
     trajectory_pts = plan.joint_trajectory.points
     last_pt = trajectory_pts[len(trajectory_pts)-1].positions
+
+    # Return degrees
+    last_pt = np.array(last_pt) * 180 / np.pi
 
     return last_pt
 
@@ -103,12 +99,15 @@ class KinematicsCalculator():
 
   def forward_kinematics(self, joint_angles):
     """
-    joint_angles- [theta1, theta2, theta3, theta4] in radians
+    joint_angles- [theta1, theta2, theta3, theta4] in degrees
                 for base_rotate, joint1, joint2, and joint3
     returns (position, angle)
       where position is [x, y, z] in spatial coordinates. in cm
             and angle is in euler angles [roll, pitch, yaw]
     """
+    joint_angles = np.array(joint_angles) * np.pi / 180
+
+    # Code after this point should use radians
     g = prod_exp(self.xis, np.array(joint_angles))
     g_ee = g.dot(self.g0) # pose of end effector
 
@@ -116,6 +115,10 @@ class KinematicsCalculator():
     euler = tf.transformations.euler_from_quaternion(quaternion)
 
     pos = g_ee[:3,3]
+
+    # Return degrees
+    euler = np.array(euler) * 180 / np.pi # radians to degrees
+
     return pos, euler
 
 def main():
@@ -130,12 +133,12 @@ def main():
   import time
   
   starttime = time.time()
-  pos, rotation = kinematics.forward_kinematics([1, 1, 1])
+  pos, rotation = kinematics.forward_kinematics([45, 30, 20])
   print('fk result', pos, rotation)
   print('fk time', time.time() - starttime)
 
   starttime = time.time()
-  print('ik result', kinematics.inverse_kinematics(pos, rotation))
+  print('ik result', kinematics.inverse_kinematics(pos))
   print('ik time', time.time() - starttime)
 
 if __name__ == '__main__':
