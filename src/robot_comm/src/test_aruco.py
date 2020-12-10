e34e47342cd1d59b6582ee8720748704d25d1179
Jenny Wang <jwang078@berkeley.edu>
2020-12-10 14:16:01 -0800

    try to decrease camera lag for aruco

--- src/robot_comm/src/test_aruco.py
+++ src/robot_comm/src/test_aruco.py
@@ -2,6 +2,18 @@ import numpy as np
 from cv2 import aruco
 import cv2
 
+import threading
+class CameraBufferCleanerThread(threading.Thread):
+    def __init__(self, camera, name='camera-buffer-cleaner-thread'):
+        self.camera = camera
+        self.last_frame = None
+        super(CameraBufferCleanerThread, self).__init__(name=name)
+        self.start()
+
+    def run(self):
+        while True:
+            ret, self.last_frame = self.camera.read()
+
 # Defined colors
 RED = (0, 0, 255)
 GREEN = (0, 255, 0)
@@ -10,6 +22,7 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
 cap = cv2.VideoCapture(0)
+cap_cleaner = CameraBufferCleanerThread(cap)
 
 aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
 parameters =  aruco.DetectorParameters_create()
@@ -20,7 +33,7 @@ class ArucoDetector():
 
     def get_pos(self):
         curr_sphere_pos = None
-        ret, frame = cap.read()
+        frame = cap_cleaner.last_frame
 
         if frame is not None:
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -44,7 +57,7 @@ class ArucoDetector():
                         0
                     ])
         
-        cv2.imshow('frame', frame)
+            cv2.imshow('frame', frame)
 
         # Is None if any of the above fails
         return curr_sphere_pos
