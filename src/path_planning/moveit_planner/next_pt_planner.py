a67f7d3fd25f9793672ab319321f9a5a92a94496
Jenny Wang <jwang078@berkeley.edu>
2020-12-10 09:22:01 -0800

    constraints for next pt planner; 2d trajectory testing

--- src/path_planning/moveit_planner/next_pt_planner.py
+++ src/path_planning/moveit_planner/next_pt_planner.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class NextPointPlanner():
-    def __init__(self, max_dist_per_timestep=2, joint_angle_ranges=[]):
+    def __init__(self, max_dist_per_timestep=2, move_const=10, z_boost=0.1, xy_boost=0.2):
         """
         Uses a handcoded algorithm to plan next point. No machine learning involved.
         When functions are called in succession, the resulting points will hopefully
@@ -12,37 +12,119 @@ class NextPointPlanner():
             - x is to the right
             - y is forwards
             - z is up
+
+        Higher move_const tends to prioritize close or far movement over facing the object
         """
         # Can control the tradeoff between centering the ball in view vs getting closer/farther from it
         self.max_dist_per_timestep = max_dist_per_timestep
+        self.move_const = move_const
+        self.z_boost = z_boost
+        self.xy_boost = xy_boost
         self.base_height = 7.5
         self.l1 = 10
         self.l2 = 12
         self.max_mag = self.l1+self.l2-2
+        self.min_mag = np.sqrt(self.l1**2+self.l2**2)+1
+        self.offset = np.array([0, 0, self.base_height]) # position of first joint
+
+    def in_arm_range(self, arm_pos, obj_pos, get_closer):
+        """
+        keep distances relative to the first joint of the robot small
+        and in ranges that the robot can actually reach
+        """
+        # can't go too far out or too close to the first joint
+        rel_arm_pos = arm_pos - self.offset
+        mag = np.linalg.norm(rel_arm_pos)
+        unit = rel_arm_pos / mag
+
+        # Need to turn instead of just clipping
+        print('unit before', unit)
+        if mag < self.min_mag + 1:
+            if not np.isclose(unit[2], 0, atol=1):
+                print('z boost')
+                # Change z to lift upwards
+                if get_closer:
+                    if arm_pos[2] > obj_pos[2]:
+                        unit[2] -= self.z_boost
+                    else:
+                        unit[2] += self.z_boost
+                else:
+                    if arm_pos[2] > obj_pos[2]:
+                        unit[2] += self.z_boost
+                    else:
+                        unit[2] -= self.z_boost
+            else:
+                print('xy boost')
+                # TODO opposite side of the middle base determines direction 
+                boost_dir = (obj_pos - self.offset)[:2]
+                boost_dir /= np.linalg.norm(boost_dir)
+                if get_closer:
+                    unit[0] += boost_dir[0] * self.xy_boost
+                    unit[1] += boost_dir[1] * self.xy_boost
+                else:
+                    unit[0] -= boost_dir[0] * self.xy_boost
+                    unit[1] -= boost_dir[1] * self.xy_boost
+
+            # boost or decrease the component to turn
+            
+            print('unit after', unit)
+            unit = unit / np.linalg.norm(unit)
+
+        clipped = np.clip(mag, self.min_mag, self.max_mag)*unit + self.offset
+
+        # can't bend below a certain point, which is z
+        clipped[2] = np.clip(clipped[2], self.base_height-self.l2, None)
+
+        return clipped
 
     def get_far_point(self, arm_pos, obj_pos):
         """
         Get target end effector position that's farther from the object than before
         using absolute coordinates, relative to the true base frame
         """
-        # Don't care about facing the object anymore
-        offset = np.array([0, 0, self.base_height])
-        rel_arm_pos = arm_pos - offset
-        rel_obj_pos = obj_pos - offset
-
-        to_obj = rel_obj_pos - rel_arm_pos
-        to_obj = to_obj / np.linalg.norm(to_obj)
-
-        return arm_pos - self.max_dist_per_timestep*to_obj + offset
+        return self._get_close_or_far_point_face_obj(arm_pos, obj_pos, get_closer=False)
 
     def get_near_point(self, arm_pos, obj_pos):
         """
         Get target end effector position that's closer to the object than before
         using absolute coordinates, relative to the initial base frame
         """
-        return self._get_close_or_far_point(arm_pos, obj_pos, get_closer=True)
+        return self._get_close_or_far_point_face_obj(arm_pos, obj_pos, get_closer=True)
 
-    def _get_close_or_far_point(self, arm_pos, obj_pos, get_closer):
+    def _get_far_point_at_obj(self, arm_pos, obj_pos):
+        """
+        Handle the case when the arm is at the object and we want to go further from it
+        Go towards the first joint position
+        """
+        get_closer = False
+        to_obj = obj_pos - self.offset
+        to_obj = to_obj / np.linalg.norm(to_obj)
+
+        target_pos = arm_pos - self.max_dist_per_timestep*to_obj
+        return self.in_arm_range(target_pos, obj_pos, get_closer)
+
+    def _get_close_or_far_point_no_face_obj(self, arm_pos, obj_pos, get_closer):
+        """
+        Don't care about facing the object 
+        """
+        # will give divide by zero for to_obj
+        if np.all(np.isclose(arm_pos, obj_pos)):
+            return self._get_far_point_at_obj(arm_pos, obj_pos)
+
+        rel_arm_pos = arm_pos - self.offset
+        rel_obj_pos = obj_pos - self.offset
+
+        to_obj = rel_obj_pos - rel_arm_pos
+        to_obj = to_obj / np.linalg.norm(to_obj)
+
+        if get_closer:
+            target_pos = arm_pos + self.max_dist_per_timestep*to_obj
+        else:
+            target_pos = arm_pos - self.max_dist_per_timestep*to_obj
+
+        return self.in_arm_range(target_pos, obj_pos, get_closer)
+
+    def _get_close_or_far_point_face_obj(self, arm_pos, obj_pos, get_closer):
         """
             Get closer or farther from the ball, while also centering the ball in view.
 
@@ -58,71 +140,83 @@ class NextPointPlanner():
         # TODO only return points that the arm can reach (defined in self.joint_angle_ranges)
 
         dist_to_obj = np.linalg.norm(arm_pos - obj_pos)
-        offset = np.array([0, 0, self.base_height])
+
+        #### BASE CASES ####
+
         if get_closer and dist_to_obj < self.max_dist_per_timestep:
-            obj_rel = obj_pos - offset
-            # Make sure the target position is not out of range
-            obj_rel_dist = min(self.max_mag, max(-self.max_mag, np.linalg.norm(obj_rel)))
-            obj_rel_vec = obj_rel_dist * (obj_rel) / np.linalg.norm(obj_rel)
-            obj_vec = obj_rel_vec + offset
+            return self.in_arm_range(obj_pos, obj_pos, get_closer)
+        if not get_closer and np.all(np.isclose(arm_pos, obj_pos)):
+            # Special case because relative rel_obj_pos has a 0 magnitude. go to the simple function
+            return self._get_far_point_at_obj(arm_pos, obj_pos)
 
-            return obj_vec
-        else:
-            # Law of cosines is
-            # c^2 = a^2 + b^2 - 2*a*b*cos(angle opposite to C)
-            # where a and b surround <angle opposite to C>
+        # Want to calculate rotations relative to the first x-axis rotary joint
+        rel_arm_pos = arm_pos - self.offset
+        rel_obj_pos = obj_pos - self.offset
 
-            # Want to calculate rotations relative to the first x-axis rotary joint
-            rel_arm_pos = arm_pos - offset
-            rel_obj_pos = obj_pos - offset
+        rel_arm_pos_unit = rel_arm_pos/np.linalg.norm(rel_arm_pos)
+        rel_obj_pos_unit = rel_obj_pos/np.linalg.norm(rel_obj_pos)
 
-            a = np.linalg.norm(rel_arm_pos)
+        if np.all(np.isclose(rel_arm_pos_unit, rel_obj_pos_unit, atol=0.05)) \
+                or np.all(np.isclose(-rel_arm_pos_unit, rel_obj_pos_unit, atol=0.05)):
+            # This type of input will give no solutions if passed into the else case
+            # The arm is lined up with the object relative to the first joint
+            return self._get_close_or_far_point_no_face_obj(arm_pos, obj_pos, get_closer)
 
-            # angle between <offset origin to end effector> and <offset origin to obj>
-            c_angle = np.arccos(np.dot(rel_arm_pos, rel_obj_pos) / np.linalg.norm(rel_arm_pos) / np.linalg.norm(rel_obj_pos))
+        #### MAIN LOGIC ####
+        # Law of cosines is
+        # c^2 = a^2 + b^2 - 2*a*b*cos(angle opposite to C)
+        # where a and b surround <angle opposite to C>
 
-            # Solve for b in b^2 - 2*a*cos(angle opposite to C)*b + (a^2 - c^2) = 0
-            def get_radicand(c):
-                # First half of the calculation for getting a target position
-                a_ = 1
-                b_ = -2*a*np.cos(c_angle)
-                c_ = a**2 - c**2
+        a = np.linalg.norm(rel_arm_pos)
 
-                # We don't want an imaginary solution
-                radicand = (b_**2 - 4*a_*c_)
-                return radicand if radicand >= 0 else None
-            def get_sol(radicand):
-                # Complete the calculation in the previous function
-                a_ = 1
-                b_ = -2*a*np.cos(c_angle)
-                # c_ is only used in radicand, which is given
+        # angle between <self.offset origin to end effector> and <self.offset origin to obj>
+        c_angle = np.arccos(np.dot(rel_arm_pos, rel_obj_pos) / np.linalg.norm(rel_arm_pos) / np.linalg.norm(rel_obj_pos))
 
-                # quadratic formula
-                b1 = min(self.max_mag, max(-self.max_mag, (-b_ + np.sqrt(radicand)) / (2*a_)))
-                b2 = min(self.max_mag, max(-self.max_mag, (-b_ - np.sqrt(radicand)) / (2*a_)))
+        # Solve for b in b^2 - 2*a*cos(angle opposite to C)*b + (a^2 - c^2) = 0
+        def get_radicand(c):
+            # First half of the calculation for getting a target position
+            a_ = 1
+            b_ = -2*a*np.cos(c_angle)
+            c_ = a**2 - c**2
 
-                # b1 and b2 are magnitudes. They lie along the line from offset origin to the offset obj
-                if get_closer:
-                    if np.linalg.norm(rel_obj_pos) > np.linalg.norm(rel_arm_pos):
-                        target_pos = b1 * (rel_obj_pos / np.linalg.norm(rel_obj_pos))
-                    else:
-                        target_pos = b2 * (rel_obj_pos / np.linalg.norm(rel_obj_pos))
+            # We don't want an imaginary solution
+            radicand = (b_**2 - 4*a_*c_)
+            return radicand if radicand >= 0 else None
+        def get_sol(radicand):
+            # Complete the calculation in the previous function
+            a_ = 1
+            b_ = -2*a*np.cos(c_angle)
+            # c_ is only used in radicand, which is given
+
+            # quadratic formula
+            b1 = (-b_ + np.sqrt(radicand)) / (2*a_)
+            b2 = (-b_ - np.sqrt(radicand)) / (2*a_)
+
+            # b1 and b2 are magnitudes. They lie along the line from self.offset origin to the self.offset obj
+            if get_closer:
+                if np.linalg.norm(rel_obj_pos) > np.linalg.norm(rel_arm_pos):
+                    return b1 * (rel_obj_pos / np.linalg.norm(rel_obj_pos))
                 else:
-                    if np.linalg.norm(rel_obj_pos) > np.linalg.norm(rel_arm_pos):
-                        target_pos = b2 * (rel_obj_pos / np.linalg.norm(rel_obj_pos))
-                    else:
-                        target_pos = b1 * (rel_obj_pos / np.linalg.norm(rel_obj_pos))
-                return target_pos
+                    return b2 * (rel_obj_pos / np.linalg.norm(rel_obj_pos))
+            else:
+                if np.linalg.norm(rel_obj_pos) > np.linalg.norm(rel_arm_pos):
+                    return b2 * (rel_obj_pos / np.linalg.norm(rel_obj_pos))
+                else:
+                    return b1 * (rel_obj_pos / np.linalg.norm(rel_obj_pos))
 
-            # Find a target position
-            # Sweep until the theoretical travel distance is large enough to give a real solution
-            target_pos = None
-            for c in np.arange(self.max_dist_per_timestep, self.max_dist_per_timestep*100, 0.1):
-                radicand = get_radicand(c)
-                if radicand is not None:
-                    target_pos = get_sol(radicand) + offset
-                    # Scale down the solution to get a distance that's in range
-                    target_pos = (target_pos - arm_pos) * (self.max_dist_per_timestep / c) + arm_pos
-                    break
+        # Find a target position
+        # Sweep until the theoretical travel distance is large enough to give a real solution
+        target_pos = None
+        for c in np.arange(self.move_const, self.move_const*100, 0.1):
+            radicand = get_radicand(c)
+            if radicand is not None:
+                target_pos = get_sol(radicand) + self.offset
+                # Scale down the solution to get a distance that's within the true max dist per timestep
+                target_pos = (target_pos - arm_pos) * (self.max_dist_per_timestep / c) + arm_pos
+                break
 
-            return target_pos
+        if target_pos is not None:
+            return self.in_arm_range(target_pos, obj_pos, get_closer)
+        else:
+            print('wot returned None for ', arm_pos, obj_pos)
+            return None
--- src/path_planning/tinyik_files/vis_planner_2d.py
+++ src/path_planning/tinyik_files/vis_planner_2d.py
@@ -1,95 +1,278 @@
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Button
 import tinyik
 import numpy as np
 import sys
 sys.path.insert(0, "/Users/himty/Desktop/Serious_Stuff/UC_Berkeley/3_Junior/EECS 106A/eecs106a-final-project/src/path_planning/moveit_planner")
 from next_pt_planner import NextPointPlanner
 
-def make_2Donclick(fig, ax, planner):
-    # yeet higher order functions
-    ee_pos = None
-    obj_pos = None
+class OnClick2D():
+    def __init__(self, planner):
+        self.planner = planner 
 
-    base_height = 7.5
-    l1 = 10
-    l2 = 12
+        ##### ROBOT STUFF ####
 
-    # 'z' and 'x' are rotation axes.
-    # Each array is the length of the joint between rotation axes
-    # I don't think this library supports prismatic joints? 
-    arm = tinyik.Actuator([
-            'z',
-            [0, 0, base_height],
-            'x',
-            [0, l1, 0],
-            'x',
-            [0, l2, 0],
-        ])
+        self.ee_pos = None
+        self.obj_pos = None
 
-    pts = []
+        self.base_height = 7.5
+        self.l1 = 10
+        self.l2 = 12
 
-    def onclick(event):
-        nonlocal ee_pos, obj_pos, pts
+        # 'z' and 'x' are rotation axes.
+        # Each array is the length of the joint between rotation axes
+        # I don't think this library supports prismatic joints? 
+        self.arm = tinyik.Actuator([
+                'z',
+                [0, 0, self.base_height],
+                'x',
+                [0, self.l1, 0],
+                'x',
+                [0, self.l2, 0],
+            ])
 
-        x, y = event.xdata, event.ydata
-        if ee_pos is None:
-            # Hardcoded z coordinate
-            ee_pos = np.array([x, y, 5])
-            print('Got end effector pos:', ee_pos)
+        self.plan_pts = []
+        self.ee_pts = []
+        self.obj_pts = []
 
-            pts.append(ax.scatter(x, y, label='End Effector'))
+        self.default_x = 0
+        self.default_y = 0
+        self.default_z = 7.5
 
-            plt.legend()
-            plt.title('Next, click where the object should go')
+        self.near_pos = np.array([float('inf') for i in range(3)])
+        self.far_pos = np.array([float('inf') for i in range(3)])
+
+        ##### PLOT STUFF ####
+
+        self.lims = [30, 30, 30]
+        self.plt_dim = (1, 3)
+        self.fig, self.ax = plt.subplots(1, 3, figsize=(13, 4))
+        for axis in range(3):
+            self.ax[axis].set_xlim([-self.lims[axis], self.lims[axis]])
+            self.ax[axis].set_ylim([-self.lims[axis], self.lims[axis]])
+
+        self.fig.canvas.mpl_connect('button_press_event', self.axis_onclick)
+
+        plt.suptitle('Click where the end effector should go')
+
+        # X squash
+        self.ax[0].title.set_text("X Into Screen (Side View)")
+        self.ax[0].scatter(0, 0, label='Origin')
+        self.ax[0].plot(*zip([0, 0], [0, self.base_height]))
+        self.ax[0].legend()
+        self.ax[0].set_xlabel('y')
+        self.ax[0].set_ylabel('z')
+
+        # Y squash
+        self.ax[1].title.set_text("Y Out of Screen (Side View)")
+        self.ax[1].scatter(0, 0, label='Origin')
+        self.ax[1].plot(*zip([0, 0], [0, self.base_height]))
+        self.ax[1].legend()
+        self.ax[1].set_xlabel('x')
+        self.ax[1].set_ylabel('z')
+
+        # Z squash
+        self.ax[2].title.set_text("Top-Down View")
+        self.ax[2].scatter(0, 0, label='Origin')
+        self.ax[2].legend()
+        self.ax[2].set_xlabel('x')
+        self.ax[2].set_ylabel('y')
+
+        self.axobjreset = plt.axes([0.93, 0.2, 0.065, 0.075])
+        self.bobjreset = Button(self.axobjreset, "Reset Obj")
+        self.bobjreset.on_clicked(self.obj_reset_onclick)
+
+        self.axfarsim = plt.axes([0.93, 0.1, 0.065, 0.075])
+        self.bfarsim = Button(self.axfarsim, 'Go Further')
+        self.bfarsim.on_clicked(self.simulate_far_onclick)
+
+        self.axnearsim = plt.axes([0.93, 0, 0.065, 0.075])
+        self.bnearsim = Button(self.axnearsim, "Go Closer")
+        self.bnearsim.on_clicked(self.simulate_near_onclick)
+
+        self.colors = {
+            'ee': 'r',
+            'obj': 'b',
+            'far': 'g',
+            'near': 'm'
+        }
+
+        self.done = True
+
+    def obj_reset_onclick(self, event):
+        if self.obj_pos is None:
+            return
+
+        self.obj_pos = None
+        [p.remove() for p in self.obj_pts]
+        [p.remove() for p in self.plan_pts]
+        self.obj_pts = []
+        self.plan_pts = []
+
+        for axis in range(3):
+            self.ax[axis].legend()
+
+        plt.suptitle('Next, click where the object should go')
+        plt.pause(0.1)
+
+    def simulate_near_onclick(self, event):
+        self.simulate_nearfar_onclick(event, is_near=True)
+
+    def simulate_far_onclick(self, event):
+        self.simulate_nearfar_onclick(event, is_near=False)
+
+    def simulate_nearfar_onclick(self, event, is_near):
+        if not self.done:
+            return
+
+        if self.ee_pos is None or self.obj_pos is None:
+            print("WARNING: Cannot simulate because end effector pos or object pos are not set. Click the plot")
+            return
+
+        if not is_near:
+            if self.far_pos is None:
+                return
+
+            self.ee_pos = self.far_pos
+        else:
+            if self.near_pos is None:
+                return
+
+            self.ee_pos = self.near_pos
+
+        [p.remove() for p in self.ee_pts]
+        [p.remove() for p in self.obj_pts]
+        [p.remove() for p in self.plan_pts]
+        self.ee_pts = []
+        self.obj_pts = []
+        self.plan_pts = []
+        self.plot_ee_pts()
+        self.plot_planner_pts()
+
+
+    def sim_mode_onclick(self, event):
+        if self.bmode.label.get_text() == self.far_mode_label:
+            self.bmode.label.set_text(self.near_mode_label) 
+        else:
+            self.bmode.label.set_text(self.far_mode_label)
+
+    def plot_ee_pts(self):
+        if not self.done:
+            return
+
+        self.done = False
+        for axis in range(3):
+            squashed_idxs = [i for i in range(3) if i != axis]
+            if axis != 2: # not z
+                self.ee_pts.append(self.ax[axis].plot(*zip([0, self.base_height],  self.ee_pos[squashed_idxs]), linestyle='-')[0])
+            else:
+                self.ee_pts.append(self.ax[axis].plot(*zip([0, 0],  self.ee_pos[squashed_idxs]), linestyle='-')[0])
+            self.ee_pts.append(self.ax[axis].scatter(*self.ee_pos[squashed_idxs], label='End Effector', color=self.colors['ee']))
+            self.ax[axis].legend()
+
+        self.done = True
+
+    def plot_planner_pts(self):
+        if self.near_pos is None or self.far_pos is None:
+            return 
+        if not self.done:
+            return
+
+        self.done = False
+        print('-')
+        self.near_pos = self.planner.get_near_point(self.ee_pos, self.obj_pos)
+        print("Near pos:", self.near_pos)
+        if self.near_pos is not None:
+            self.arm.ee = self.near_pos
+            print("Near pos's joint angles:", self.arm.angles)
+        else:
+            print("WARNING: near_pos returned None for ee_pos {} and obj_pos {}".format(self.ee_pos, self.obj_pos))
+
+        self.far_pos = self.planner.get_far_point(self.ee_pos, self.obj_pos)
+        print("Far pos:", self.far_pos)
+        if self.far_pos is not None:
+            self.arm.ee = self.far_pos
+            print("Far pos's joint angles:", self.arm.angles)
+        else:
+            print("WARNING: far_pos returned None for ee_pos {} and obj_pos {}".format(self.ee_pos, self.obj_pos))
+
+        for axis in range(3):
+            squashed_idxs = [i for i in range(3) if i != axis]
+            self.obj_pts.append(self.ax[axis].scatter(*self.obj_pos[squashed_idxs], label='Object', color=self.colors['obj']))
+            
+            if self.near_pos is not None:
+                self.plan_pts.append(self.ax[axis].scatter(*self.near_pos[squashed_idxs], label='Near', color=self.colors['near']))
+                self.plan_pts.append(self.ax[axis].plot(*zip(self.ee_pos[squashed_idxs], self.near_pos[squashed_idxs]), linestyle=':')[0])
+        
+            if self.far_pos is not None:
+                self.plan_pts.append(self.ax[axis].scatter(*self.far_pos[squashed_idxs], label='Far', color=self.colors['far']))
+                self.plan_pts.append(self.ax[axis].plot(*zip(self.ee_pos[squashed_idxs], self.far_pos[squashed_idxs]), linestyle=':')[0])
+
+            self.ax[axis].legend()
+        plt.pause(0.1)
+        self.done = True
+
+
+    def axis_onclick(self, event):
+        axis_idx = -1
+        for axis in range(3):
+            if self.ax[axis] == event.inaxes:
+                axis_idx = axis
+                break
+
+        ax_pos = self.ax[axis_idx].transAxes.inverted().transform((event.x, event.y))
+        ax_pos = [min(1, max(0, coord))*self.lims[axis]*2-self.lims[axis] for axis, coord in enumerate(ax_pos)]
+
+        if axis_idx == 0: # x is squished
+            pt = np.array([self.default_x, ax_pos[0], ax_pos[1]])
+        elif axis_idx == 1: # y is squished
+            pt = np.array([ax_pos[0], self.default_y, ax_pos[1]])
+        elif axis_idx == 2:
+            pt = np.array([ax_pos[0], ax_pos[1], self.default_z])
+        else:
+            return # Probably a click to the button
+
+        if self.ee_pos is None:
+            self.ee_pos = np.array(pt)
+            print('Got end effector pos:', self.ee_pos)
+
+            # Display for all three angles
+            self.plot_ee_pts()
+
+            plt.suptitle('Next, click where the object should go')
             plt.pause(0.1)
-        elif obj_pos is None:
+        elif self.obj_pos is None:
             # Hardcoded z coordinate
-            obj_pos = np.array([x, y, 5])
-            print('Got obj pos:', obj_pos)
+            self.obj_pos = np.array(pt)
+            print('Got obj pos:', self.obj_pos)
 
-            pts.append(plt.scatter(x, y, label='Object'))
-            plt.pause(0.1)
+            self.plot_planner_pts()
 
-            near_pos = planner.get_near_point(ee_pos, obj_pos)
-            pts.append(plt.scatter(*near_pos[:2], label='Near'))
-            pts.append(plt.plot(*zip(ee_pos[:2], near_pos[:2]), linestyle=':')[0])
-            print("Near pos:", near_pos)
-            arm.ee = near_pos
-            print("Near pos's joint angles:", arm.angles)
-
-            far_pos = planner.get_far_point(ee_pos, obj_pos)
-            pts.append(plt.scatter(*far_pos[:2], label='Far'))
-            pts.append(plt.plot(*zip(ee_pos[:2], far_pos[:2]), linestyle=':')[0])
-            print("Far pos:", far_pos)
-            arm.ee = far_pos
-            print("Far pos's joint angles:", arm.angles)
-
-            plt.legend()
-            plt.title('Plotted planner points. Click the figure again to clear the screen')
+            plt.suptitle('Plotted planner points. Click the figure again to clear the screen')
             plt.pause(0.1)
         else:
-            [p.remove() for p in pts]
-            pts = []
-            plt.legend()
-            plt.title('Click where the end effector should go')
+            [p.remove() for p in self.ee_pts]
+            [p.remove() for p in self.obj_pts]
+            [p.remove() for p in self.plan_pts]
+            self.ee_pts = []
+            self.obj_pts = []
+            self.plan_pts = []
+            for axis in range(3):
+                self.ax[axis].legend()
+            plt.suptitle('Click where the end effector should go')
             plt.pause(0.1)
 
-            ee_pos = None
-            obj_pos = None
+            self.ee_pos = None
+            self.obj_pos = None
+            self.near_pos = np.array([float('inf') for i in range(3)])
+            self.far_pos = np.array([float('inf') for i in range(3)])
 
             print('Cleared.')
             print('\n---------------------\n')
-    return onclick
 
 if __name__ == "__main__":
-    planner = NextPointPlanner(2, [])
+    planner = NextPointPlanner()
 
-    fig,ax = plt.subplots()
-    ax.set_xlim([-30, 30])
-    ax.set_ylim([-30, 30])
-    fig.canvas.mpl_connect('button_press_event', make_2Donclick(fig, ax, planner))
+    vis = OnClick2D(planner)
 
-    plt.title('Click where the end effector should go')
-    plt.scatter(0, 0, label='Origin')
-    plt.legend()
     plt.show()
